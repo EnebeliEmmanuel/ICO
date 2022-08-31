04bf061990f45a9b1ff52e739b074d9a035ce658
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2022-08-31 01:21:52 +0100

    [CHORE] Initial-coin-offering

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x015d3252cCCbf44C26abD1114b574698Db4D46EA";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
